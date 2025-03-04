4ad3a4068dc41bb58ce88af8c603b98cd7509eea
Andre Guerra <andre.guerra@mail.mcgill.ca>
2025-03-04 14:17:31 -0500

    updated docustring description, added annotation to plot

--- python/visualization/subplots.py
+++ python/visualization/subplots.py
@@ -1,6 +1,19 @@
 """This script contains an example for making subploit in matplotlib.
+
+Contents:
+Visualization
+1. Visualization constants
+2. Adaptable subplots structure (NORWS x NCOLS, and standard figure size)
+3. Text annotation on the plot
+4. Save fgiure in different file formats
+5. Dynamic zooming in plots
+
+Data handling:
+1. Read in data using Pandas DataFrame
+2. Find indices of columns in the DataFrame
 """
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -61,6 +74,8 @@ axs[0].set_ylabel('Temperature',fontsize=TITLEFONT)
 axs[0].tick_params(axis='x',labelsize=TICKSFONT)
 axs[0].tick_params(axis='y',labelsize=TICKSFONT)
 axs[0].set_ylim(zoomLims(thermo,temp,ZOOMOUT))
+axs[0].annotate('T$_{avg}$ = ' + str(np.around(thermo.iloc[:,temp].mean(),decimals=2)),
+                xy=(0.45,0.2), xycoords='axes fraction', fontsize=TEXTFONT)
 
 axs[1].plot(thermo.iloc[:,t],thermo.iloc[:,temp])
 axs[1].plot(thermo.iloc[:,t],aveT)
